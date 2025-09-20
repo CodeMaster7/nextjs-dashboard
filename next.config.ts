ee045f204f76b8dae669c7354640090ebcc8a710
Sam <sammybonfanti@gmail.com>
2025-09-20 15:01:12 -0700

    removed ppr

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	// Disable PPR to fix Vercel deployment database connection issues
-	experimental: {
-		ppr: 'incremental'
-	}
+	/* config options here */
 }
 
 export default nextConfig
